f7632d0622f34024bd6046a257c72acc02308f35
Matt <matiaslucero21884@gmail.com>
2025-01-04 21:02:57 -0300

    Imagen ubicacion

---
+++ script.js
@@ -0,0 +1,45 @@
+let menuVisible = false;
+/* Funcion que oculta o muestra el menu */
+function mostrarOcultarMenu(){
+    if (menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;   
+    }else {
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true; 
+    }
+}
+
+function seleccionar(){
+    /* Oculto el menu una vez que selecciono una opcion */
+    document.getElementById("nav").classList ="";
+    menuVisible = false;
+}
+/* Funcion que aplica las animaciones de las habilidades */
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >=300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("html");
+        habilidades[1].classList.add("css");
+        habilidades[2].classList.add("javascript");
+        habilidades[3].classList.add("react");
+        habilidades[4].classList.add("java");
+
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajoenequipo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proactividad");
+        habilidades[10].classList.add("empatia");
+        habilidades[11].classList.add("colaboracion");
+        
+        
+    }
+}
+/* Detecto el escrolling para aplicar la animacion de la barra de habilidades */
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
